47bec9a505d49b921572b48bcdf5cb30d9ed302c
Tatoxa <Tatoxer@gmail.com>
2024-12-17 19:56:47 +0300

    Added script

---
+++ enumerate_picture_names.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+
+current_dir = Path.cwd()
+BASE_NAME = 'pict_'
+count = 1
+
+for file in current_dir.iterdir():
+    if file.is_file() and file.stem != "enumerate_picture_names":
+        new_name = f'{BASE_NAME}{count}{file.suffix}'
+        new_path = file.with_name(new_name)
+        file.rename(new_path)
+        count += 1
