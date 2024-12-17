2723435d5f1436e1727474cafec82f352a9d30cf
Tatoxa <Tatoxer@gmail.com>
2024-12-17 20:01:30 +0300

    bugfix

--- enumerate_picture_names.py
+++ enumerate_picture_names.py
@@ -6,8 +6,12 @@ BASE_NAME = 'pict_'
 count = 1
 
 for file in current_dir.iterdir():
-    if file.is_file() and file.stem != "enumerate_picture_names":
-        new_name = f'{BASE_NAME}{count}{file.suffix}'
-        new_path = file.with_name(new_name)
-        file.rename(new_path)
+    try:
+        if file.is_file() and file.stem != "enumerate_picture_names":
+            new_name = f'{BASE_NAME}{count}{file.suffix}'
+            new_path = file.with_name(new_name)
+            file.rename(new_path)
+            count += 1
+    except Exception as e:
         count += 1
+        continue
